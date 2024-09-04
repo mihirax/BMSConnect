2073740f32e7d2d804d7d692b4767cf55ea942cf
mihirax <117086670+mihirax@users.noreply.github.com>
2024-09-04 12:18:16 +0530

    Update api.js

--- client/src/Service/api.js
+++ client/src/Service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const usersUrl = 'http://localhost:3003/users';
-const usersUrl = 'http://localhost:5000';
+const usersUrl = 'https://bmsc-r4g1.vercel.app/index.js';
 
 export const getUsers = async (id) => {
     id = id || '';
