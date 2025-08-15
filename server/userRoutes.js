9710a79e95c6612c2a52a091642def50402a8f03
Mihira <117086670+mihirax@users.noreply.github.com>
2025-08-15 15:45:37 +0530

    Delete server directory

--- server/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addUser } = require('./user-controller');
-
-router.post('/users', addUser);
-
-module.exports = router;
